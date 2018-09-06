cf30827bf0ff5338dd2352039c7a0490c3cc5f65
aygulmalikova <amalikova19@gmail.com>
2018-09-06 21:43:24 +0300

    Landing page edits

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,15 +1,47 @@
 const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.nav-menu');
-const icon = document.querySelector('#nav-icon');
-const hero = document.querySelector('.hero');
+const navMenu = document.querySelector('.nav-menu');
+const link = document.querySelectorAll('.link');
+const body = document.body;
 
 hamburger.addEventListener('click', function () {
-    menu.classList.toggle('open');
-    icon.classList.toggle('open');
-    hero.classList.toggle('close');
-    document.body.classList.toggle('menu-open');
-    document.body.classList.add('menu-animation');
-    setTimeout(() => {
-        document.body.classList.remove('menu-animation');
-    }, 700);
+
+    body.classList.toggle('open');
+    if (body.classList.contains('open')) {
+        fadeIn(navMenu, "flex");
+    } else {
+        fadeOut(navMenu);
+    }
 });
+
+for (let i = 0; i < link.length; i++) {
+    link[i].addEventListener('click', function () {
+        body.classList.remove('open');
+    })
+}
+
+function fadeOut(el){
+    el.style.opacity = 1;
+
+    (function fade() {
+        if ((el.style.opacity -= .05) < 0) {
+            el.style.display = "none";
+        } else {
+            requestAnimationFrame(fade);
+        }
+    })();
+}
+
+// fade in
+
+function fadeIn(el, display){
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+
+    (function fade() {
+        var val = parseFloat(el.style.opacity);
+        if (!((val += .05) > 1)) {
+            el.style.opacity = val;
+            requestAnimationFrame(fade);
+        }
+    })();
+}
